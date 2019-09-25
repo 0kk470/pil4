3e55821ebebe0f570106750df8201c7aa088ffc5
0kk470 <ookk47oo@gmail.com>
2019-09-25 11:11:05 +0800

    Lua and Readme's auto generator

---
+++ chapterCreator.lua
@@ -0,0 +1,43 @@
+local iChapterNum = 33
+
+local function file_exists(name)
+   local f = io.open(name,"r")
+   if f then
+      io.close(f)
+	  return true
+   end
+   return false
+end
+
+local function createLua(filename)
+   os.execute("mkdir " .. filename)
+   local path = string.format("%s/%s.lua",filename,filename)
+   if file_exists(path) then
+      print("Existed file")
+      return
+   end
+   local file = io.output(path)
+   if file then
+      print("success")
+      file:write("--TODO")
+   end
+end
+
+for i = 1,iChapterNum do
+   createLua("chapter"..i)
+end
+
+
+local function updateReadMe()
+   local f = io.open("README.md","w")
+   local fmtText = "[chapter%d](https://github.com/0kk470/Lua-4th/blob/master/chapter%d/chapter%d.lua)"
+   if f then
+      f:write("# Lua-4th\n\nLua程序设计第四版习题答案","\n","\n")
+      for i = 1,iChapterNum do
+         f:write(string.format(fmtText,i,i,i),"\n","\n")
+      end
+	  return true
+   end
+   return false
+end
+updateReadMe()
