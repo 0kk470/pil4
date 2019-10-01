7e9ba1e8861fe1efe2f83bf751fe862f142b9e82
0kk470 <ookk47oo@gmail.com>
2019-10-01 22:42:56 +0800

    chapter10 test

--- chapter10/chapter10.lua
+++ chapter10/chapter10.lua
@@ -1,1 +1,26 @@
 --TODO
+
+
+-- local s = "hello world"
+
+-- local i , j = string.find(s,"hello")
+
+-- print(string.sub(s,i,j))
+
+-- local date = "today is 2019/10/1"
+
+-- print(string.match(date,"%d+/%d+/%d+"))
+
+
+
+-- print(string.gsub("i am a silly boy","a","p"))
+
+-- string.find(str,"^d")          --检查字符串s是否以数字开头
+
+-- string.find(str,"^[+-]?%d+$")  --检查字符串是否为一个没有多余前缀字符和后缀字符的整数
+
+-- print(  string.gsub("a (enclosed (in) parenthese) line","%b()","")  )
+
+
+local s =  "the anthem is the theme"
+print( string.gsub(s,"%f[%w]the%f[%W]","one") )
