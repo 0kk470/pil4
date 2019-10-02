6e8176cb4901288ae31617d02cd7d0650b349f41
0kk470 <ookk47oo@gmail.com>
2019-10-02 23:30:19 +0800

    update chapter10

--- chapter10/chapter10.lua
+++ chapter10/chapter10.lua
@@ -1,26 +1,50 @@
---TODO
+---练习10.1
+---use find and sub
+function string.split(str, sep)
+    local result = {}
+    if not str or str == "" then
+        return result
+    end
+    if sep == "" then
+        result[#result + 1] = str
+        return result
+    end
+    sep = sep or " "
+    local start, length = 1, #str
+    while start <= length do
+        local i, j = string.find(str, sep, start)
+        if not i or not j then
+            if start <= length then
+                result[#result + 1] = string.sub(str, start, length)
+            end
+            break
+        end
+        result[#result + 1] = string.sub(str, start, i - 1)
+        start = j + 1
+    end
+    return result
+end
 
+--use pattern
+function string.split2(str, sep)
+    local result = {}
+    sep = sep or " "
+    string.gsub(str, string.format("([^%s]+)", sep), function(c)
+        result[#result + 1] = c
+    end)
+    return result
+end
 
--- local s = "hello world"
-
--- local i , j = string.find(s,"hello")
-
--- print(string.sub(s,i,j))
-
--- local date = "today is 2019/10/1"
-
--- print(string.match(date,"%d+/%d+/%d+"))
-
-
-
--- print(string.gsub("i am a silly boy","a","p"))
-
--- string.find(str,"^d")          --检查字符串s是否以数字开头
-
--- string.find(str,"^[+-]?%d+$")  --检查字符串是否为一个没有多余前缀字符和后缀字符的整数
-
--- print(  string.gsub("a (enclosed (in) parenthese) line","%b()","")  )
-
-
-local s =  "the anthem is the theme"
-print( string.gsub(s,"%f[%w]the%f[%W]","one") )
+local function test10_1()
+    local test = "123|||||456|780|"
+    local a = string.split(test, "|")
+    local b = string.split2(test, "|")
+    for i = 1, #a do
+        print(a[i])
+    end
+    print("----------------")
+    for i = 1, #b do
+        print(b[i])
+    end
+end
+test10_1()
