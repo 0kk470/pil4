786c7fe322fe2634e70ccffa2aa87542a82ac59a
0kk470 <ookk47oo@gmail.com>
2019-10-09 21:56:48 +0800

    update chapter13

--- chapter13/chapter13.lua
+++ chapter13/chapter13.lua
@@ -1,20 +1,22 @@
---- 练习13.1 请编写一个函数，实现无符号整形的模运算
+local function isinteger(n)
+    return math.type(n) == "integer"
+end
+--- 练习13.1 请编写一个函数，实现无符号整型数的取模运算
 
 --- 练习13.2 请用不同的方式实现对Lua整形数值的二进制的位数计算
 
---- 练习13.3 你怎么检测一个给定的整数是2的幂次方
-local function check2(n)
-    return n ~ 0 == n
+--- 练习13.3 如何判断一个给定的整数是2的幂次方?
+local function checkpower2(n)
+    assert(isinteger(n))
+    return  n > 0 and n & (n - 1) == 0
 end
-print(check2(1))
-print(check2(2))
-print(check2(3))
-print(check2(4))
-print(check2(5))
+for i = 1,2^5 do
+   print(i,checkpower2(i))
+end
 
-
---- 练习13.4 编写一个函数来计算一个给定整数的汉明权值（一个数字的汉明权值是它的二进制表示下的1的个数）
+--- 练习13.4 编写一个函数来计算一个指定整数的汉明权重（一个数的汉明权重是其二进制表示中的1的个数）
 local function hammingweight(n)
+    assert(isinteger(n))
     local iCount = 0
     while n ~= 0 do
         n = n & (n - 1)
@@ -23,12 +25,21 @@ local function hammingweight(n)
     return iCount
 end
 print(hammingweight(1))
-print(hammingweight(2))
+print(hammingweight(3))
 print(hammingweight(4))
-print(hammingweight(7))
-
+print(hammingweight(math.tointeger(2^32)))
 
 --- 练习13.5 编写一个函数来测试一个给定整数的二进制表示是否是一个回文数
+--local function ispali(n)
+--    assert(isinteger(n))
+--    local o = n
+--    local a = {}
+--    while n ~= 0 and n ~= 1 do
+--        a[#a + 1] = math.modf()
+--    end
+--    print(tonumber(n,2))
+--end
+--print(tonumber("11",2))
 
 --- 练习13.6 用lua实现一个bit数组。它应该支持下列操作
 --- ● newBitArray(n) (创建一个有n个bit的数组)
