9c087f2a94533eabbedc1ee7ba5072d6523da024
0kk470 <ookk47oo@gmail.com>
2019-10-15 21:45:49 +0800

    update ch15

--- chapter15/chapter15.lua
+++ chapter15/chapter15.lua
@@ -1,4 +1,9 @@
---Example
+--- 练习15.1 || 练习15.2 ||  练习15.3 || 练习15.4
+
+local function isIdentifier(i)
+   return string.match(i, "^[a-zA-Z_][a-zA-Z0-9_]*") == i
+end
+
 local function serialize(o,indentation)
     indentation = indentation or 0
     local t = type(o)
@@ -8,12 +13,22 @@ local function serialize(o,indentation)
         io.write("\n")
         local space = string.rep(" ",indentation + 2)
         local space2 = string.rep(" ",indentation + 4)
+        local saved = {}
         if indentation > 0 then io.write(space) end
         io.write("{\n")
-        for k,v in pairs(o) do
-            io.write(space2,k," = ")
-            serialize(v,indentation + 2)
+        for i = 1,#o  do
+            io.write(space2)
+            serialize(o[i],indentation + 2)
             io.write(",\n")
+            saved[i] = true
+        end
+        for k,v in pairs(o) do
+            if not saved[k] then
+                local key = isIdentifier(k) and k or string.format('[%q]',k)
+                io.write(space2,key," = ")
+                serialize(v,indentation + 2)
+                io.write(",\n")
+            end
         end
         if indentation > 0 then io.write(space,"}")
         else  io.write("}\n") end
@@ -22,44 +37,82 @@ local function serialize(o,indentation)
     end
 end
 
+local testTable = {
+    a = 1,
+    b = "str",
+    c =
+    {
+        d = 1,
+        e = true,
+        f = {},
+    },
+    g = {1,2,3},
+    "a",
+    "b",
+    "c",
+    {7,8,9},
+    ["#"] = "identifier",
+}
+io.input("data.txt","w")
+io.output("data.txt")
+serialize(testTable)
+
+
 local function basicSerialize(o)
     -- number or string
     return string.format("%q",o)
 end
 
-local function save(name,value,saved)
+local function save(name,value,saved,indentation)
+    indentation = indentation or 0
     saved = saved or {}
+    local t = type(value)
+    --local space = string.rep(" ",indentation + 2)
+    --local space2 = string.rep(" ",indentation + 4)
+    --if indentation > 0 then io.write(space) end
     io.write(name," = ")
-    if type(value) == "number" or type(value) == "string" then
+    if t == "number" or t == "string"  or t == "boolean" or t == "nil" then
         io.write(basicSerialize(value),"\n")
-    elseif type(value) == "table" then
+    elseif t == "table" then
         if saved[value] then
             io.write(saved[value],"\n")
         else
+            local indexes = {}
+            if #value > 0 then io.write("{\n") end
+            for i = 1,#value do
+                local fname = string.format("%s[%s]",name,i)
+                if type(value[i]) ~= "table" then
+                    io.write(basicSerialize(value[i]))
+                else
+                    save(fname,value[i],saved,indentation + 2)
+                end
+                saved[value[i]] = fname
+                io.write(",\n")
+                indexes[i] = true
+            end
+            if #value > 0 then
+                io.write("}\n")
+            else
+                io.write("{}\n")
+            end
             saved[value] = name
-            io.write("{}\n")
             for k,v in pairs(value) do
-                k = basicSerialize(k)
-                local fname = string.format("%s[%s]",name,k)
-                save(fname,v,saved)
+                if not indexes[k] then
+                    k = basicSerialize(k)
+                    local fname = string.format("%s[%s]",name,k)
+                    save(fname,v,saved,indentation + 2)
+                    io.write("\n")
+                end
             end
         end
     else
-        error("cannot save a " .. type(value))
+        error("cannot save a " .. t)
     end
 end
 
-
-local testTable = {
-    a = 1,
-    b = "str",
-    c = {
-        d = 1,
-        e = true,
-        f = {},
-    }
-}
-
-io.input("data.txt","w")
-io.output("data.txt")
-serialize(testTable)
+---TODO
+--local a = {"one","Two",3,{4,5,6,7}}
+--local b = { k = a[1]}
+--local t = {}
+--save("a",a,t)
+--save("b",b,t)
