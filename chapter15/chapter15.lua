b76dfd6870e4beab6a2b0c668fa409799e4d4402
0kk470 <ookk47oo@gmail.com>
2019-10-15 19:45:11 +0800

    update chapter15

--- chapter15/chapter15.lua
+++ chapter15/chapter15.lua
@@ -1,16 +1,22 @@
 --Example
-local function serialize(o)
+local function serialize(o,indentation)
+    indentation = indentation or 0
     local t = type(o)
     if t == "number" or t == "string" or t == "boolean" or t == "nil" then
         io.write(string.format("%q",o))
     elseif t == "table" then
+        io.write("\n")
+        local space = string.rep(" ",indentation + 2)
+        local space2 = string.rep(" ",indentation + 4)
+        if indentation > 0 then io.write(space) end
         io.write("{\n")
         for k,v in pairs(o) do
-            io.write(" ",k," = ")
-            serialize(v)
+            io.write(space2,k," = ")
+            serialize(v,indentation + 2)
             io.write(",\n")
         end
-        io.write("}\n")
+        if indentation > 0 then io.write(space,"}")
+        else  io.write("}\n") end
     else
         error("cannot serialize a " .. type(o))
     end
@@ -42,3 +48,18 @@ local function save(name,value,saved)
         error("cannot save a " .. type(value))
     end
 end
+
+
+local testTable = {
+    a = 1,
+    b = "str",
+    c = {
+        d = 1,
+        e = true,
+        f = {},
+    }
+}
+
+io.input("data.txt","w")
+io.output("data.txt")
+serialize(testTable)
