a61b4b78c8d09c966022143330af47259b8d54d6
0kk470 <ookk47oo@gmail.com>
2019-10-17 22:59:17 +0800

    update chapter16

--- chapter16/chapter16.lua
+++ chapter16/chapter16.lua
@@ -1,29 +1,42 @@
 
- local function f(x,y,z)
-     return x + y + z
+---练习16.1
+ local function loadwithprefix(prefix,code)
+     local type_prefix = type(prefix)
+     local type_code = type(code)
+     assert(type_prefix == "string","invalid prefix:" .. type_prefix)
+     assert(type_code == "string" or type_code == "function","invalid code type:" .. type_code)
+     local func = nil
+     if type(code) == "function" then
+         func = code
+     else
+         local isLoad = false
+         func = function()
+             if isLoad then return nil end
+             isLoad = true
+             return code
+         end
+     end
+     local isprefixload = false
+     local loader = function()
+         if isprefixload then return func() end
+         isprefixload = true
+         return prefix
+     end
+     return load(loader,"prefix loader","t")
  end
 
- local function callback(str)
-     print("callback:" .. tostring(str))
-     return "i am a error"
+ local f1 = loadwithprefix("return ","1 + 1")
+ print(f1())
+ local count = 0
+ local iter = function ()
+     while count < 5 do
+         count = count + 1
+         return count
+     end
  end
+ local f2 = loadwithprefix("return ",iter)
+ print(f2())
 
- local ret,result = xpcall(f,callback,2,2,nil)
- if ret then
-     print(result)
- else
-     print("error message:" .. tostring(result))
- end
+local function multiload(...)
 
-local function foo(str)
-    if type(str) ~= "string" then
-        error("string expected",2)
-    end
 end
-
- local function test()
-     foo({ a = "table"})
- end
-
- test()
-
