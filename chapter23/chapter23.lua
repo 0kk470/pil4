3c20224cfbc12bba067bfd21ebbfa150418ef619
0kk470 <ookk47oo@gmail.com>
2019-11-04 22:09:13 +0800

    chapter23 finished

--- chapter23/chapter23.lua
+++ chapter23/chapter23.lua
@@ -22,7 +22,8 @@ end
 
 
 
----练习23.2
+---练习23.2 考虑23.6节的第一个例子，该示例创建了一个带有析构器的表，该析构器再执行时只是输出一条消息。
+---如果程序没有进行过垃圾收集就退出会发生什么?如果程序调用os.exit()呢?如果程序由于出错而退出呢？
 local function test23_2()
     local o = {x = "hi"}
     setmetatable(o, {__gc = function (o) print(o.x) end})
@@ -35,7 +36,8 @@ end
 
 
 
----练习23.3
+---练习23.3 假设要实现一个记忆表，该记忆表所对应函数的参数和返回值都是字符串。由于弱引用表不把字符串当作可回收对象，
+---因此将这个记忆表标记为弱引用并不能使得其中的键值对能够被垃圾收集。在这种情况下，该如何实现记忆功能呢？
 
 --用一张表映射字符串就可以了
 local function test23_3()
@@ -59,7 +61,7 @@ end
 
 --test23_3()
 
---- 练习23.4 解释示例23.3程序的输出。
+--- 练习23.4 解释示例23.3中程序的输出。
 local function figure23_3()
     local count = 0
     local mt = {__gc = function () count = count - 1 end}
@@ -85,8 +87,47 @@ local function figure23_3()
     print(collectgarbage("count") * 1024, count)
 end
 
-figure23_3()
+--figure23_3()
 
 
 
---- 练习23.5 
+--- 练习23.5 对于这个练习，你需要一个使用大量内存的Lua脚本。如果没有可以写一个这样的脚本（一个创建表的循环就行）。
+--- ● 使用不同的payse和stepmul参数运行脚本。它们的值是如何影响脚本的性能和内存使用的?
+--- 如果把pause值设为0会发生什么？把pause设成1000会发生什么?把stepmul设成0会发生什么？stepmul设成1000000会发生什么？
+--- ● 调整你的脚本，使其能够完全控制垃圾收集器。脚本应该让垃圾收集器停止运行，然后时不时地完成垃圾收集的工作。
+--- 你能够使用这种方式提高脚本的性能吗?
+local function isWindows()
+    return package.config:sub(1,1) == "\\"
+end
+
+local function sleep(n)
+    if isWindows() then
+        if n > 0 then os.execute("ping -n " .. tonumber(n+1) .. " localhost > NUL") end
+    else
+        os.execute("sleep " .. tonumber(n))
+    end
+end
+
+local function allocMemory()
+    local a = {}
+    for i = 1,100000 do
+        a[i] = {}
+    end
+end
+
+--setpause 在达到上一次垃圾收集器完成时内存的pause比例前，垃圾收集器会处于暂停状态 比如200表示达到上一次回收结束时内存的两倍(200%)才会进行一次新的垃圾收集
+--setstepmul 每分配1kb内存 执行对应stepmul比例的垃圾回收处理
+local function test23_5()
+    print(collectgarbage("count") * 1024)
+    --collectgarbage("setstepmul",100000)
+    --collectgarbage("setpause",0)
+    --collectgarbage("setpause",1000)
+    --collectgarbage("setstepmul",1000000)
+    while true do
+        sleep(1)
+        allocMemory()
+        print(collectgarbage("count") * 1024)
+    end
+end
+
+--test23_5()
