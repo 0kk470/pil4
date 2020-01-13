64ffff15cc1c744d6d3f77fff4552dcd100f883a
0kk470 <ookk47oo@gmail.com>
2020-01-13 22:08:30 +0800

    update chapter30

--- chapter30/chapter30.cpp
+++ chapter30/chapter30.cpp
@@ -91,9 +91,119 @@ static int l_filter(lua_State* L) //Exercise 30.1
     return 1;
 }
 
+static int l_split(lua_State *L) //Exercise 30.2
+{
+    const char *s = luaL_checkstring(L, 1);
+    const char *sep = luaL_checkstring(L, 2);
+    int len = luaL_len(L, 1);
+    const char *e;
+    int i = 1;
+    lua_newtable(L);
+    while( (e = (char*)memchr(s, *sep, len)) != NULL)
+    {
+        lua_pushlstring(L, s, e - s);
+        lua_rawseti(L, -2, i++);
+        len -= e - s;
+        s = e + 1;
+    }
+    lua_pushstring(L, s);
+    lua_rawseti(L, -2 , i);
+
+    return 1;
+}
+
+/* Exercise 30.3 ~ Exercise 30.4 */
+static char key = 'k';
+int l_settrans_reg(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TTABLE);
+    lua_rawsetp(L,LUA_REGISTRYINDEX,(void *)&key);
+    return 0;
+}
+
+int l_gettrans_reg(lua_State *L)
+{
+    int t = lua_rawgetp(L,LUA_REGISTRYINDEX,(void *)&key);
+    if(t != LUA_TTABLE) 
+        error(L, "You have not set a table for function 'transliterate', gettrans failed!");
+    return 1;
+}
+
+int l_transliterate_reg(lua_State *L) 
+{
+    const char *s = luaL_checkstring(L, -1);
+    size_t l = luaL_len(L, -1);
+    const char *end = s + l;
+    luaL_Buffer b;
+    luaL_buffinitsize(L, &b, l);
+    char key[2] = {'\0','\0'};
+    l_gettrans_reg(L);
+    luaL_checktype(L, -1 , LUA_TTABLE);
+    int t  = LUA_TNIL;
+    while(s != end)
+    {
+        strncpy(key, s, 1);
+        t = lua_getfield(L, -1, key);
+        switch (t)
+        {
+            case LUA_TSTRING:
+                luaL_addstring(&b,lua_tostring(L, -1));
+                break;
+            case LUA_TBOOLEAN:
+                if(lua_toboolean(L, -1) != 0)
+                    luaL_addchar(&b, *s);
+                break;
+            default:
+                luaL_addchar(&b, *s);
+                break;
+        }
+        lua_pop(L, 1);
+        ++s;
+    }
+    lua_settop(L, 0);
+    luaL_pushresult(&b);
+    return 1;
+}
+
+/* Exercise 30.5 */
+int l_settrans_upvalue(lua_State *L)
+{
+    luaL_checktype(L, 1, LUA_TTABLE);
+    lua_copy(L, -1, lua_upvalueindex(1) );
+    return 0;
+}
+
+int l_gettrans_upvalue(lua_State *L)
+{
+    lua_pushvalue(L, lua_upvalueindex(1));
+    if(!lua_istable(L, -1))
+        error(L, "You have not set a table for function 'transliterate', gettrans failed!");
+    return 1;
+}
+
+int trans_closure(lua_State *L)
+{
+    return 1;
+}
+
+int l_transliterate_upvalue(lua_State *L) 
+{
+    lua_pushcclosure(L, trans_closure, 1);
+    return 1;
+}
+
 static const struct luaL_Reg mylib [] =
 {
     {"filter",l_filter},
+    {"split",l_split},
+    /* 使用注册表 */
+    {"settrans",l_settrans_reg},
+    {"gettrans",l_gettrans_reg},
+    {"transliterate",l_transliterate_reg},
+    /* 使用上值 */
+    // {"settrans",l_settrans_upvalue},
+    // {"gettrans",l_gettrans_upvalue},
+    // {"transliterate",l_transliterate_upvalue},
     {NULL,NULL},
 };
 
--- chapter30/chapter30.lua
+++ chapter30/chapter30.lua
@@ -13,11 +13,19 @@ print(t2[1], t2[2])
 
 ---Exercise 30.2: Modify the function l_split (from Figure 30.2, “Splitting a string”) so that it can work
 ---with strings containing zeros. (Among other changes, it should use memchr instead of strchr.)
-
-
+local t = mylib.split("a|b|cccc\0|d",'|')
+local str = "a|b|cccc\0|d"
+print(str)
+for i,v in ipairs(t) do
+    print(v)
+end
 
 ---Exercise 30.3: Reimplement the function transliterate (Exercise 10.3) in C.
-
+local char_tbl = { ["-"] = " ",["?"] = false}
+print(mylib.settrans(char_tbl))
+print(mylib.gettrans())
+print(char_tbl)
+print(#mylib.transliterate("Lua-is-a-good-language-for-me?????????"))
 
 
 ---Exercise 30.4: Implement a library with a modification of transliterate so that the transliteration
@@ -36,3 +44,5 @@ print(t2[1], t2[2])
 
 ---Exercise 30.6: Do you think it is a good design to keep the transliteration table as part of the state of the
 ---library, instead of being a parameter to transliterate?
+
+---是一种好的设计，如果状态不是经常改变的话可以节省函数的调用开销，也减少了对新表的构造。
