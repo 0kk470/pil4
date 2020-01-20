94eb7fd0c012a2eb035fa1193d5e25ddf2b75742
0kk470 <ookk47oo@gmail.com>
2020-01-20 22:46:19 +0800

    update chapter32

--- chapter32/chapter32.cpp
+++ chapter32/chapter32.cpp
@@ -1,3 +1,6 @@
+
+// remember to put libexpat.a into you mingw's lib
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -8,6 +11,7 @@ extern "C"
 #include "../lua_src/lauxlib.h"
 #include "../lua_src/lualib.h"
 }
+#include "./expat/expat.h"
 
 void error(lua_State *L, const char *fmt,...)
 {
@@ -56,8 +60,8 @@ static int dir_iter(lua_State *L)
     {
         if(d) 
         {
+            closedir(d);
             d = NULL;
-            lua_remove(L, lua_upvalueindex(1));
         }
         return 0;
     }
@@ -93,11 +97,177 @@ void luaL_opendirlib(lua_State *L)
     lua_pop(L, 1); 
 }
 
+/* XML Parser */
+typedef struct lxp_userdata{
+    XML_Parser parser;
+    lua_State *L;
+}lxp_userdata;
+
+static void f_StartElement(void *ud,const char *name, const char **atts);
+
+static void f_CharData(void *ud, const char *s, int len);
+
+static void f_EndElement(void *ud, const char *name);
+
+static int lxp_make_parser(lua_State *L)
+{
+    XML_Parser p;
+
+    lxp_userdata *xpu = (lxp_userdata*)lua_newuserdata(L, sizeof(lxp_userdata));
+
+    // Initialize
+    xpu->parser = NULL;
+
+    // set metatable
+    luaL_getmetatable(L, "Expat");
+    lua_setmetatable(L, -2);
+
+    // create expat parser
+    p = xpu->parser = XML_ParserCreate(NULL);
+    if(!p)
+      error(L,"XML_ParserCreate failed!");
+
+    // check and save callback table
+    luaL_checktype(L, 1, LUA_TTABLE);
+    lua_pushvalue(L, 1);
+    lua_setuservalue(L, -2);
+
+    // set expat parser
+    XML_SetUserData(p, xpu);
+    XML_SetElementHandler(p, f_StartElement, f_EndElement);
+    XML_SetCharacterDataHandler(p, f_CharData);
+    return 1;
+}
+
+static int lxp_parse(lua_State *L)
+{
+    int status;
+    size_t len;
+    const char *s;
+    lxp_userdata *xpu;
+
+    xpu = (lxp_userdata *)luaL_checkudata(L, 1, "Expat");
+
+    luaL_argcheck(L, xpu->parser != NULL, 1, "parser is closed");
+
+    s = luaL_optlstring(L, 2, NULL, &len);
+
+    lua_settop(L, 2);
+    lua_getuservalue(L, 1);
+
+    xpu->L = L;
+
+    status = XML_Parse(xpu->parser, s, (int)len, s == NULL);
+
+    lua_pushboolean(L, status);
+    return 1;
+}
+
+static void f_CharData(void *ud, const char *s, int len)
+{
+    lxp_userdata *xpu = (lxp_userdata *)ud;
+    lua_State *L = xpu->L;
+
+    lua_getfield(L, 3, "CharacterData");
+    if(lua_isnil(L, -1))
+    {
+        lua_pop(L, 1);
+        return;
+    }
+    lua_pushvalue(L, 1);
+    lua_pushlstring(L, s, len);
+    lua_call(L, 2, 0);
+}
+
+static void f_StartElement(void *ud, const char *name, const char **atts)
+{
+    lxp_userdata *xpu = (lxp_userdata *)ud;
+    lua_State *L = xpu->L;
+    lua_getfield(L, 3, "CharacterData");
+    if (lua_isnil(L, -1))
+    {
+        lua_pop(L, 1);
+        return;
+    }
+    lua_pushvalue(L, 1);
+    lua_pushstring(L, name);
+
+    lua_newtable(L);
+    for(; *atts; atts += 2)
+    {
+        lua_pushstring(L, *(atts + 1));
+        lua_setfield(L, -2, *atts);
+    }
+
+    lua_call(L, 3, 0);
+}
+
+static void f_EndElement(void *ud ,const char *name)
+{
+    lxp_userdata *xpu = (lxp_userdata *)ud;
+
+    lua_State *L = xpu->L;
+
+    lua_getfield(L, 3, "EndElement");
+    if(lua_isnil(L, -1))
+    {
+        lua_pop(L, 1);
+        return;
+    }
+    lua_pushvalue(L, 1);
+    lua_pushstring(L, name);
+    lua_call(L, 2, 0);
+}
+
+static int lxp_close(lua_State *L)
+{
+    lxp_userdata *xpu = (lxp_userdata *)luaL_checkudata(L, 1, "Expat");
+
+    if(xpu->parser)
+    {
+        XML_ParserFree(xpu->parser);
+        xpu->parser = NULL;
+        return 0;
+    }
+}
+
+static const luaL_Reg lxp_meths[] = {
+    {"parse", lxp_parse},
+    {"close", lxp_close},
+    {"__gc", lxp_close},
+    {NULL, NULL},
+};
+
+static const luaL_Reg lxp_funcs[] = {
+    {"new", lxp_make_parser},
+    {NULL, NULL},
+};
+
+int luaopen_lxp(lua_State *L)
+{
+    luaL_newmetatable(L, "Expat");
+
+    lua_pushvalue(L, -1);
+    lua_setfield(L, -2, "__index");
+
+    luaL_setfuncs(L, lxp_meths, 0);
+
+    luaL_newlib(L, lxp_funcs);
+    return 1;
+}
+
+void luaL_openlxplib(lua_State *L)
+{
+    luaL_requiref(L,"lxp",luaopen_lxp,1);
+    lua_pop(L, 1); 
+}
+
 int main()
 {
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
     luaL_opendirlib(L);
+    luaL_openlxplib(L);
     if(luaL_dofile(L, "chapter32.lua") != LUA_OK)
     {
         printf("lua error: %s",lua_tostring(L,-1));
--- chapter32/chapter32.lua
+++ chapter32/chapter32.lua
@@ -8,6 +8,15 @@ for fname in dir.open(".") do
     print(fname)
 end
 
+local parser = lxp.new()
+local t = parser:parse(
+[[
+    <Button name = "clickBtn"></Button>
+]]
+)
+for k,v in pairs(t) do
+    print(k,v)
+end
 
 -- Exercise 32.2: In the lxp example, we used user values to associate the callback table with the userdata
 -- that represents a parser. This choice created a small problem, because what the C callbacks receive is the
