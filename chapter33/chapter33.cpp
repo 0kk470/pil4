96e2a4f3cc0c35b984bcb98b4f5c7f063da8b25e
0kk470 <ookk47oo@gmail.com>
2020-01-30 14:53:27 +0800

    update chapter33

--- chapter33/chapter33.cpp
+++ chapter33/chapter33.cpp
@@ -50,7 +50,58 @@ static void movevalues(lua_State* send, lua_State* rec)
     int n = lua_gettop(send);
     luaL_checkstack(rec, n, "too many results");
     for(int i = 2; i <= n; i++)
-        lua_pushstring(rec, lua_tostring(send, i));
+    {
+        int t = lua_type(send, i);
+        switch (t)
+        {
+            case LUA_TSTRING:
+            {
+                lua_pushstring(rec, lua_tostring(send, i));
+                break;
+            }
+            case LUA_TNUMBER:
+            {
+                lua_pushnumber(rec, lua_tonumber(send, i));
+                break;
+            }
+            case LUA_TBOOLEAN:
+            {
+                lua_pushboolean(rec, lua_toboolean(send, i));
+                break;
+            }
+            case LUA_TFUNCTION:
+            {
+                lua_pushcfunction(rec, lua_tocfunction(send, i));
+                break;
+            }
+            case LUA_TUSERDATA:
+            {
+                lua_pushlightuserdata(rec, lua_touserdata(send, i));
+                break;
+            }
+            case LUA_TLIGHTUSERDATA:
+            {
+                lua_pushlightuserdata(rec, lua_touserdata(send, i));
+                break;
+            }
+            case LUA_TTHREAD:
+            {
+                lua_pushthread(lua_tothread(send, i));
+                break;
+            }
+            case LUA_TNIL:
+            {
+                lua_pushnil(rec);
+                break;
+            }
+            case LUA_TTABLE:
+            {
+                break;
+            }
+            default:
+                break;
+        }
+    }
 }
 
 static Proc* searchmatch(const char *channel, Proc **list)
@@ -91,28 +142,116 @@ static void waitonlist(lua_State *L, const char *channel, Proc **list)
     }while(p->channel);
 }
 
-static const struct luaL_Reg mylib [] =
+static int ll_send(lua_State *L)
 {
-    {NULL,NULL},
-};
-
-LUAMOD_API int luaopen_mylib(lua_State* L)
-{
-    luaL_newlib(L, mylib);
-    return 1;
+    Proc *p;
+    const char *channel = luaL_checkstring(L, 1);
+    pthread_mutex_lock(&kernel_access);
+    p = searchmatch(channel, &waitreceive);
+    if(p)
+    {
+        movevalues(L, p->L);
+        p->channel = NULL;
+        pthread_cond_signal(&p->cond);
+    }
+    else
+    {
+        waitonlist(L, channel, &waitsend);
+    }
+    pthread_mutex_unlock(&kernel_access);
+    return 0;
 }
 
-void luaL_openmylib(lua_State* L)
+static int ll_receive(lua_State *L)
 {
-    luaL_requiref(L,"mylib",luaopen_mylib,1);
-    lua_pop(L, 1); 
+    Proc *p;
+    const char *channel = luaL_checkstring(L, 1);
+    lua_settop(L, 1);
+
+    pthread_mutex_lock(&kernel_access);
+    p = searchmatch(channel, &waitsend);
+    if(p)
+    {
+        movevalues(p->L, L);
+        p->channel = NULL;
+        pthread_cond_signal(&p->cond);
+    }
+    else
+    {
+        waitonlist(L, channel, &waitreceive);
+    }
+    
+}
+
+
+int luaopen_lproc(lua_State *L);
+
+static void *ll_thread(void *arg)
+{
+    lua_State *L = (lua_State *)arg;
+    Proc *self;
+
+    luaL_openlibs(L);
+    luaL_requiref(L, "lproc", luaopen_lproc, 1);
+    lua_pop(L, 1);
+    self = (Proc *)lua_newuserdata(L, sizeof(Proc));
+    lua_setfield(L, LUA_REGISTRYINDEX, "_SELF");
+    self->L = L;
+    self->thread = pthread_self();
+    self->channel = NULL;
+    pthread_cond_init(&self->cond, NULL);
+
+    if(lua_pcall(L, 0, 0, 0) != LUA_OK)
+        error(L, "thread error: %s", lua_tostring(L, -1));
+
+    pthread_cond_destroy(&getself(L)->cond);
+    lua_close(L);
+    return NULL; 
+}
+
+static int ll_start(lua_State *L)
+{
+    pthread_t thread;
+    const char *chunk = luaL_checkstring(L, 1);
+    lua_State *L1 = luaL_newstate();
+
+    if(L1 == NULL)
+        error(L, "unable to create new state");
+    if(luaL_loadstring(L1, chunk) != LUA_OK)
+        error(L, "error in thread body: %s", lua_tostring(L1, -1));
+    if(pthread_create(&thread, NULL, ll_thread, L1) != LUA_OK)
+        error(L, "unable to create new thread"); 
+    pthread_detach(thread);
+    return 0;
+}
+
+static int ll_exit(lua_State *L)
+{
+    pthread_exit(NULL);
+    return 0;
+}
+
+static const struct luaL_Reg ll_funcs[] =
+{
+    {"start", ll_start},
+    {"send", ll_send},
+    {"receive", ll_receive},
+    {"exit", ll_exit},
+    {NULL, NULL},
+};
+
+LUAMOD_API int luaopen_lproc(lua_State* L)
+{
+    luaL_newlib(L, ll_funcs);
+    return 1;
 }
 
 int main()
 {
     lua_State *L = luaL_newstate();
     luaL_openlibs(L);
-    luaL_openmylib(L);
+    luaL_requiref(L, "lproc", luaopen_lproc, 1);
+    lua_pop(L, 1);
     if(luaL_dofile(L, "chapter33.lua") != LUA_OK)
     {
         printf("lua error: %s",lua_tostring(L,-1));
--- chapter33/chapter33.lua
+++ chapter33/chapter33.lua
@@ -1,1 +1,33 @@
 --TODO
+function Sleep(n)
+    if n > 0 then os.execute("ping -n " .. tonumber(n + 1) .. " localhost > NUL") end
+end
+
+local co = coroutine.wrap(function(x)
+    coroutine.yield(1)
+    print(x)
+    coroutine.yield(2)
+    print(x)
+    coroutine.yield(3)
+    print(x)
+    print("coroutine end")
+end)
+print(1)
+print(2)
+lproc.start([[
+print("c1 receive begin")
+lproc.receive("c1")
+print("c1 received ,receive c2")
+lproc.receive("c2")
+print("receive c2 end")
+]])
+lproc.start([[
+print("send c1 begin")
+lproc.send("c1")
+print("send c1 end, start send c2")
+Sleep(3)
+lproc.send("c2")
+print("c2 sended")
+]])
+print(3)
+io.read()
