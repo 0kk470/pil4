72e37e5aa9e5970c6a71b4230c79d65e0617abea
0kk470 <2549366978@qq.com>
2021-11-09 03:57:40 +0800

    fix exercise 17.3 problem: https://github.com/0kk470/pil4/issues/7

--- chapter17/chapter17.lua
+++ chapter17/chapter17.lua
@@ -11,16 +11,32 @@
 ---练习17.2 将几何区域系统的实现（示例9.4）重写为恰当的模块
 local geometry = require("geometry")
 local square = geometry.rect(-1,1,-1,1)
-geometry.plot( geometry.difference( square,geometry.rotate(square,45) ),100,100)
+geometry.plot( geometry.difference(square, geometry.rotate(square,45)), 100, 100)
 
 
 
 ---练习17.3 如果库搜索路径中包含固定的路径组成（即没有包含问号）会发生什么?这一行为有用吗?
-package.path = package.path .. ";D:/LuaTest/chapter16/chapter16.lua"  --换成你自己电脑上的绝对路径
-local chapter16 = require("chapter16")
-if chapter16 then print("chapter16 require success") end
--- 有用 Lua会在搜索其他路径失败的情况下根据固定路径搜索对应的文件
+package.path = package.path .. ";D:/pil4/chapter17/geometry.lua"
+local testmod1 = require("D:/pil4/chapter17/geometry.lua")
+local testmod2 = require("D:/pil4/chapter17/geometry.lua")
+local mod1 = require("somethingNotExist1")
+local mod1_copy = require("somethingNotExist1")
+local mod2 = require("somethingNotExist2")
+local mod3 = require("somethingNotExist3")
+print("testmod1: ".. tostring(testmod1))
+print("testmod2: ".. tostring(testmod2))
+print("mod1: ".. tostring(mod1))
+print("mod2 ".. tostring(mod2))
+print("mod3 ".. tostring(mod3))
+print("testmod1 == testmod2: ".. tostring(testmod1 == testmod2))
+print("testmod1 == mod1: ".. tostring(testmod1 == mod1))
+print("mod1 == mod1_copy: ".. tostring(mod1 == mod1_copy))
+print("testmod1 == mod1: ".. tostring(testmod1 == mod1))
 
+-- 如果在lua的搜索path中添加一项绝对路径的文件名，会导致require一个不存在的模块时，这个模块都会搜索到固定文件名的文件作为它导入的模块
+-- 并且如果模块名不一样，缓存的table也不一样，就算加载了相同固定路径的module，在内存中也是两份独立的副本
+-- 在极端情况下还是有用，相当于始终会有默认的模块被加载，可以用这套机制来拓展require函数
+-- 详细可参见 https://www.lua.org/pil/8.1.html
 
 
 ---练习17.4 编写一个同时搜索Lua文件和C标准库的搜索器。
