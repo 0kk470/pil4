c8accc0a32e73dbeb142baeadf64f350a7ea7837
0kk470 <2549366978@qq.com>
2023-06-22 12:01:04 +0800

    fix: exercise 5.8

--- chapter05/chapter05.lua
+++ chapter05/chapter05.lua
@@ -136,9 +136,10 @@ end
 
 function table.my_fastConcat(t) --这个效率会高的多
    local result = {}
-   local temp = nil
+   local temp, str
    for i = 1,#t do
-      temp = { string.byte(tostring(t[i])) } 
+      str = tostring(t[i])
+      temp = { string.byte(tostring(t[i]), 1, #str) } 
       for j = 1,#temp do
 	     result[#result + 1] = temp[j]
 	  end
@@ -149,17 +150,18 @@ end
 local function test5_8()
     local a = {}
     for i = 1,200000 do
-       a[i] = "a"
+       a[i] = "abc"
     end
     local startTime = os.time()
-    table.concat(a)
+    local str1 = table.concat(a)
     print("table.concat cost Time :" .. os.time() - startTime .. "s")
     startTime = os.time()
-    table.my_slowConcat(a)
+    local str2 = table.my_slowConcat(a)
     print("table.my_slowConcat cost Time :" .. os.time() - startTime .. "s")
     startTime = os.time()
-    table.my_fastConcat(a)
+    local str3 = table.my_fastConcat(a)
     print("table.my_fastConcat cost Time :" .. os.time() - startTime .. "s")
+    assert(str1 == str2 and str2 == str3, "check str1 str2 str3 failed")
 end
 test5_8()
 
